e3fd53a2b645e5fc36d958479899884fc21039c6
MITHU ROY <ajmithuroy@gmail.com>
2025-12-31 00:38:45 +0600

    update login step defination

--- src/test/java/eal/step_definitions/Login_StepD.java
+++ src/test/java/eal/step_definitions/Login_StepD.java
@@ -16,38 +16,14 @@ public class Login_StepD extends CommonMethods {
 	}
 
 	@Then("Verify {string} is visible")
-	  public void verify_visible_string(String string_value_form_feature) {
-		  boolean validationStatus = false;
-		  
-		  switch(string_value_form_feature) {
-		  	case "UserID":
-		  		 logger.info("Verifying presense of"+string_value_form_feature+"text");
-				  boolean useridisVisible = hmpage_pom.verify_home_page_elements(string_value_form_feature);
-				  validationStatus = useridisVisible;
-				  break;
-		  		
-		  	case "Password":
-		  		 logger.info("Verifying presense of "+string_value_form_feature+"text");
-				  boolean passwordisVisible = hmpage_pom. hmpage_pom.verify_home_page_elements(string_value_form_feature);
-				  validationStatus = passwordisVisible;
-		  		break;
-		  	
-		  	case "LOGIN":
-		  		 logger.info("Verifying presense of login button");
-				  boolean loginBtnisVisible = hmpage_pom.verify_loginBtn_isVisible();
-				  validationStatus =loginBtnisVisible;
-		  		break;
-		  		
-		  	default:
-		  		logger.info("No Value is matched");		  		
-		  		break;
-		  		
-		  }
-		  logger.info("Performing Assertion");
-		  softAssert.softAssertTrue(validationStatus, 
-				  string_value_form_feature + " Text is Visible in the screen", 
-				  string_value_form_feature +" text is not visible in the screen");
-	  }
+	public void verify_visible_string(String string_value_form_feature) {
+		logger.info("Verifying presense of" + string_value_form_feature + "text");
+		boolean validationStatus = hmpage_pom.verify_home_page_elements(string_value_form_feature);
+
+		logger.info("Performing Assertion");
+		softAssert.softAssertTrue(validationStatus, string_value_form_feature + " Text is Visible in the screen",
+				string_value_form_feature + " text is not visible in the screen");
+	}
 
 	@Then("Click on Selenium Drop down from the top")
 	public void click_on_selenium_drop_down_from_the_top() {
